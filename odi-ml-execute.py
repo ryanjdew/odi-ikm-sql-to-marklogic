d3b6808b5dd5c2bc97a9ae489b9fbc5275567d55
Nick LaPrell <Nick.LaPrell@marklogic.com>
2018-06-05 09:52:41 -0700

    Fixes caused by mutli-row support. Updated to override user option FORMAT of XML to JSON when defining a record identifier to create JSON specific output.

--- odi-ml-execute.py
+++ odi-ml-execute.py
@@ -2,35 +2,32 @@ from java.lang import String
 from com.marklogic.client import *
 from jarray import array
 rowCount = rowCount + 1
+newRecordId = ""
 
 # Get the record ID for the current row
-newRecordId = ""
 <%=odiRef.getColList("", "if \u0022[CX_COL_NAME]\u0022 == recordIdentifier: newRecordId = \u0022#[CX_COL_NAME]\u0022", "\n", "", "(INS and !TRG)")%>
 
 # Initialize recordId if this is the first record
 if newRecordId != "" and recordId == "": recordId = newRecordId
 
-# Add the record to the batch writer when a new record ID is encountered
-if recordId != newRecordId:
+# Add the previous record to the batch writer when a new record ID is encountered
+if not multiRow or recordId != newRecordId:
   addToBatch(output)
   # Reset output and the recordId
   output = ""
   recordId = newRecordId
 
-<% if (odiRef.getOption("FORMAT").equals("JSON")) { %>
-if output != "": output = output + ","
-output = output + "{ "
-<%=odiRef.getColList("", "output = output + \u0022\\\u0022[CX_COL_NAME]\\\u0022 : \\\u0022#[CX_COL_NAME]\\\u0022", ",\u0022\n", "\u0022", "(INS and !TRG)")%>
-output = output + "}"
-extension = "json"
-<% } else { %>
-# TODO: Fix XML root for multiple row records.
-doc = builder.newDocument()
-results = doc.createElement("<%=odiRef.getOption("XML_ROOT")%>")
-doc.appendChild(results)
-<%=odiRef.getColList("", "node = doc.createElement(\u0022[CX_COL_NAME]\u0022)\nnode.appendChild(doc.createTextNode(\u0022#[CX_COL_NAME]\u0022))\nresults.appendChild(node)", "\n", "", "(INS and !TRG)")%>
-sw = StringWriter()
-transformer.transform(DOMSource(doc), StreamResult(sw))
-output = output + sw.getBuffer().toString()
-extension = "xml"
-<% } %>
+if outputFormat == "JSON":
+  if output != "": 
+    output = output + ","
+  output = output + "{ "
+  <%=odiRef.getColList("", "output = output + \u0022\\\u0022[CX_COL_NAME]\\\u0022 : \\\u0022#[CX_COL_NAME]\\\u0022", ",\u0022\n  ", "\u0022", "(INS and !TRG)")%>
+  output = output + "}"
+else:
+  doc = builder.newDocument()
+  results = doc.createElement("<%=odiRef.getOption("XML_ROOT")%>")
+  doc.appendChild(results)
+  <%=odiRef.getColList("", "node = doc.createElement(\u0022[CX_COL_NAME]\u0022)\n  node.appendChild(doc.createTextNode(\u0022#[CX_COL_NAME]\u0022))\n  results.appendChild(node)", "\n  ", "", "(INS and !TRG)")%>
+  sw = StringWriter()
+  transformer.transform(DOMSource(doc), StreamResult(sw))
+  output = output + sw.getBuffer().toString()
--- odi-ml-init.py
+++ odi-ml-init.py
@@ -11,23 +11,35 @@ from javax.xml.transform.stream import *
 from com.marklogic.client import *
 from com.marklogic.client.datamovement import *
 from org.w3c.dom import *
+
 # Open a file for logging
 print >> open("<%=odiRef.getOption("LOG_FILE")%>", 'w'), strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 log = open("<%=odiRef.getOption("LOG_FILE")%>", 'a')
-# Determine the file format for incoming documents
+print >> log, "File Format: <%=odiRef.getOption("FORMAT")%>"
+
+# Initialize variables used later
 fileFormat = io.Format.JSON if "<%=odiRef.getOption("FORMAT")%>" == "JSON" else io.Format.XML
-# Set our unique record identifier
+outputFormat = "<%=odiRef.getOption("FORMAT")%>"
 recordId = ""
 recordIdentifier = "<%=odiRef.getOption("RECORD_IDENTIFIER")%>"
-# Initialize our output string
+if recordIdentifier == "": 
+  multiRow = False 
+else: multiRow = True
 output = ""
-print >> log, "File Format: <%=odiRef.getOption("FORMAT")%>"
+
+if multiRow: print >> log, "Multi row support enabled. Records identified by: " + recordIdentifier
+if multiRow and outputFormat == "XML": 
+  outputFormat = "JSON"
+  print >> log, "WARNING: Multi row support enabled but FORMAT option set to XML. This setting will be ignored."
+
 # Create a database connection
 auth = DatabaseClientFactory.DigestAuthContext("<%=odiRef.getOption("ML_USER")%>","<%=odiRef.getOption("ML_PASSWORD")%>");
 client = DatabaseClientFactory.newClient("<%=odiRef.getOption("ML_HOST")%>", int("<%=odiRef.getOption("ML_PORT")%>"), auth);
 print >> log, "Created a database connection to <%=odiRef.getOption("ML_HOST")%>:<%=odiRef.getOption("ML_PORT")%> with user: <%=odiRef.getOption("ML_USER")%>."
+
 # Use DataMovementManager
 manager = client.newDataMovementManager();
+
 # Setup a write batcher to be used to batch process files.
 writer = manager.newWriteBatcher();
 writer.withJobName("IKM Import");
@@ -38,11 +50,9 @@ writer.withBatchSize(int("<%=odiRef.getOption("BATCH_SIZE")%>"));
 class successListener(WriteBatchListener):
   def __init__(self,fn):
     self.processEvent=fn
-
 @successListener
 def batchSuccess(batch):
   print >> log, str(batch.getTimestamp().time) + " - SUCCESS (Batch: " + str(batch.getJobBatchNumber()) + ")"
-
 writer.onBatchSuccess(batchSuccess)
 <% } %>
 
@@ -51,20 +61,18 @@ writer.onBatchSuccess(batchSuccess)
 class failListener(WriteFailureListener):
   def __init__(self,fn):
     self.processEvent=fn
-
 @failListener
 def batchFail(batch):
   print >> log, str(batch.getTimestamp().time) + " - FAIL (Batch: " + str(batch.getJobBatchNumber()) + ")"
-
 writer.onBatchFailure(batchFail)
 <% } %>
 
-<% if (odiRef.getOption("FORMAT").equals("XML")) { %>
 # Use a transformer for XML
-tf = TransformerFactory.newInstance()
-transformer = tf.newTransformer()
-transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes")
-<% } %>
+if outputFormat == "XML":
+  tf = TransformerFactory.newInstance()
+  transformer = tf.newTransformer()
+  transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes")
+
 # Start the job to be used while processing files.
 job = manager.startJob(writer);
 docFactory = DocumentBuilderFactory.newInstance()
@@ -72,10 +80,13 @@ builder = docFactory.newDocumentBuilder()
 
 rowCount = 0
 
+# Function to add the output as a file to the batch writer
 def addToBatch(output):
-  <% if (odiRef.getOption("FORMAT").equals("JSON")) { %>
-  output = "[" + output + "]"
-  <% } %>
+  if outputFormat == "JSON":
+    extension = "json"
+    if multiRow: output = "[" + output + "]"
+  else:
+    extension = "xml"
   output = output.encode('utf-8')
   stringHandle = io.StringHandle(output);
   stringHandle.withFormat(fileFormat);
